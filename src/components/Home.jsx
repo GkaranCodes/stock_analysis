97b828cfe6af5cd5c64ffe77d6aa8c0a62fdd17f
GkaranCodes <gkaran2112@gmail.com>
2025-04-06 15:48:09 +0530

    add edit and delete functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { formatDateForInput } from "../utils/helpers";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const Home = () => {
@@ -7,7 +8,6 @@ const Home = () => {
     stockName: "",
     dateFrom: "",
     dateTo: "",
-    direction: "",
     result: "",
   });
 
@@ -25,6 +25,32 @@ const Home = () => {
   const [analyses, setAnalyses] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const [editingId, setEditingId] = useState(null);
+  const [editing, setEditing] = useState(false);
+
+  const [editingDetails, setEditingDetails] = useState({
+    date: "",
+    name: "",
+    directionPlanned: "",
+    actualMove: "",
+    analysisResult: "",
+    remarks: "",
+  });
+
+  useEffect(() => {
+    const record = allAnalyses.find((i) => i._id === editingId);
+    if (record) {
+      setEditingDetails({
+        date: formatDateForInput(record.date) || "",
+        name: record.name || "",
+        directionPlanned: record.directionPlanned || "",
+        actualMove: record.actualMove || "",
+        analysisResult: record.analysisResult || "",
+        remarks: record.remarks || "",
+      });
+    }
+  }, [editingId, allAnalyses]);
 
   useEffect(() => {
     fetchAnalyses();
@@ -45,12 +71,50 @@ const Home = () => {
       setLoading(false);
     }
   };
-  
+
   const handleFilterChange = (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const handleEditChange = (e) => {
+    setEditingDetails({
+      ...editingDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleEditAnalysis = async (id) => {
+    setEditing(true);
+    try {
+      const response = await axios.put(
+        `${BASE_URL}/edit-analysis?id=${id}`,
+        editingDetails
+      );
+      if (response.status == 200) {
+        console.log("Data Updated Successfully");
+      }
+      await fetchAnalyses();
+      setEditingId(null);
+    } catch (error) {
+      console.log("Failed to update data");
+    } finally {
+      setEditing(false);
+    }
+  };
+
+  const handleDeleteAnalysis = async (id) => {
+    try {
+      const res = await axios.delete(`${BASE_URL}/delete-analysis?id=${id}`);
+      if (res.ok) {
+        console.log("Data deleted successfully");
+      }
+      await fetchAnalyses();
+    } catch (error) {
+      console.log("Failed to delete data", error?.message);
+    }
   };
 
   const handleAddChange = (e) => {
@@ -60,38 +124,51 @@ const Home = () => {
     });
   };
 
+  const handleClearFilter = async (e) => {
+    e.preventDefault();
+    try {
+      setFormData({
+        stockName: "",
+        dateFrom: "",
+        dateTo: "",
+        direction: "",
+        result: "",
+      });
+      await fetchAnalyses();
+    } catch (error) {
+      console.log("Something went wrong");
+    }
+  };
+
   const handleFilter = (e) => {
     e.preventDefault();
-  
+
     const filtered = allAnalyses.filter((item) => {
       const { stockName, dateFrom, dateTo, direction, result } = formData;
-  
-      const matchesStock = stockName ? item.name.toLowerCase().includes(stockName.toLowerCase()) : true;
-      const matchesDirection = direction ? item.directionPlanned === direction : true;
+
+      const matchesStock = stockName
+        ? item.name.toLowerCase().includes(stockName.toLowerCase())
+        : true;
       const matchesResult = result ? item.analysisResult === result : true;
-      
+
       const itemDate = new Date(item.date);
       const fromDate = dateFrom ? new Date(dateFrom) : null;
       const toDate = dateTo ? new Date(dateTo) : null;
-  
+
       const matchesDate =
-        (!fromDate || itemDate >= fromDate) &&
-        (!toDate || itemDate <= toDate);
-  
+        (!fromDate || itemDate >= fromDate) && (!toDate || itemDate <= toDate);
+
       return matchesStock && matchesDirection && matchesResult && matchesDate;
     });
-  
+
     setAnalyses(filtered);
   };
-  
+
   const submitForm = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await axios.post(
-        `${BASE_URL}/create-analysis`,
-        newAnalysis
-      );
+      const res = await axios.post(`${BASE_URL}/create-analysis`, newAnalysis);
 
       if (res.status === 200 || res.status === 201) {
         setNewAnalysis({
@@ -279,23 +356,6 @@ const Home = () => {
                   className="w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 />
               </div>
-
-              <div className="flex-1">
-                <label className="block text-sm font-medium text-gray-700 mb-1">
-                  Direction
-                </label>
-                <select
-                  name="direction"
-                  value={formData.direction}
-                  onChange={handleFilterChange}
-                  className="w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                >
-                  <option value="">All</option>
-                  <option value="bullish">Bullish</option>
-                  <option value="bearish">Bearish</option>
-                  <option value="sideways">Sideways</option>
-                </select>
-              </div>
               <div className="flex-1">
                 <label className="block text-sm font-medium text-gray-700 mb-1">
                   Result
@@ -312,7 +372,7 @@ const Home = () => {
                 </select>
               </div>
 
-              <div>
+              <div className="flex gap-3">
                 <button
                   type="submit"
                   className="py-2 px-6 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition-colors duration-200 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2"
@@ -320,10 +380,132 @@ const Home = () => {
                 >
                   {loading ? "Filtering..." : "Filter Results"}
                 </button>
+                <button
+                  onClick={handleClearFilter}
+                  className="py-2 px-6 bg-green-600 focus:ring-offset-indigo-200 text-white transition-colors duration-200 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2"
+                  disabled={loading}
+                >
+                  Clear Filters
+                </button>
               </div>
             </div>
           </form>
         </div>
+        {editingId && (
+          <div className="bg-white rounded-lg shadow-lg p-6 mb-8">
+            <h2 className="text-xl font-semibold text-white pl-5 bg-gradient-to-r bg-sky-400 to-white rounded-xl">
+              Edit Analysis
+            </h2>
+            <form className="flex flex-wrap justify-between items-end gap-4 mt-2">
+              <div className="flex flex-col justify-start">
+                <label
+                  className="font-semibold text-gray-500 mb-1"
+                  htmlFor="date"
+                >
+                  Date
+                </label>
+                <input
+                  type="date"
+                  name="date"
+                  value={editingDetails.date || ""}
+                  onChange={handleEditChange}
+                  className="border border-gray-300 p-2 rounded-lg outline-none focus:ring-2 focus:ring-indigo-200"
+                />
+              </div>
+              <div className="flex flex-col justify-start">
+                <label
+                  className="font-semibold text-gray-500 mb-1"
+                  htmlFor="name"
+                >
+                  Stock
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  value={editingDetails.name || ""}
+                  onChange={handleEditChange}
+                  className="border border-gray-300 p-2 rounded-lg outline-none focus:ring-2 focus:ring-indigo-200"
+                  placeholder="Enter stock name"
+                />
+              </div>
+              <div className="flex flex-col justify-start">
+                <label
+                  className="font-semibold text-gray-500 mb-1"
+                  htmlFor="directionPlanned"
+                >
+                  Direction Planned
+                </label>
+                <input
+                  type="text"
+                  name="directionPlanned"
+                  onChange={handleEditChange}
+                  value={editingDetails.directionPlanned || ""}
+                  className="border border-gray-300 p-2 rounded-lg outline-none focus:ring-2 focus:ring-indigo-200"
+                  placeholder="Enter planned direction"
+                />
+              </div>
+              <div className="flex flex-col justify-start">
+                <label
+                  className="font-semibold text-gray-500 mb-1"
+                  htmlFor="actualMove"
+                >
+                  Actual Move
+                </label>
+                <input
+                  type="text"
+                  name="actualMove"
+                  value={editingDetails.actualMove || ""}
+                  onChange={handleEditChange}
+                  className="border border-gray-300 p-2 rounded-lg outline-none focus:ring-2 focus:ring-indigo-200"
+                  placeholder="Enter actual move"
+                />
+              </div>
+              <div className="flex flex-col justify-start">
+                <label
+                  className="font-semibold text-gray-500 mb-1"
+                  htmlFor="analysisResult"
+                >
+                  Result
+                </label>
+                <select
+                  name="analysisResult"
+                  value={editingDetails.analysisResult || ""}
+                  onChange={handleEditChange}
+                  className="border border-gray-300 p-2 rounded-lg outline-none focus:ring-2 focus:ring-indigo-200"
+                >
+                  <option value="">Select result</option>
+                  <option value="correct">Correct</option>
+                  <option value="incorrect">Incorrect</option>
+                </select>
+              </div>
+              <div className="flex flex-col justify-start min-w-[200px]">
+                <label
+                  className="font-semibold text-gray-500 mb-1"
+                  htmlFor="remarks"
+                >
+                  Remarks
+                </label>
+                <input
+                  type="text"
+                  name="remarks"
+                  value={editingDetails.remarks || ""}
+                  onChange={handleEditChange}
+                  className="border border-gray-300 p-2 rounded-lg outline-none focus:ring-2 focus:ring-indigo-200"
+                  placeholder="Enter remarks"
+                />
+              </div>
+              <div className="mt-2">
+                <button
+                  onClick={() => handleEditAnalysis(editingId)}
+                  className="py-2 px-6 bg-indigo-600 hover:bg-indigo-700 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition duration-200"
+                  disabled={editing}
+                >
+                  {editing ? "Saving..." : "Save"}
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
         <div className="bg-white rounded-lg shadow-lg overflow-hidden">
           <div className="flex justify-between items-center p-4 border-b">
             <h2 className="text-xl font-semibold text-gray-800">
@@ -383,6 +565,18 @@ const Home = () => {
                     >
                       Remarks
                     </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Edit
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Delete
+                    </th>
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
@@ -440,6 +634,22 @@ const Home = () => {
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                           {analysis.remarks || "-"}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          <button
+                            onClick={() => setEditingId(analysis._id)}
+                            className="bg-green-500 text-white px-4 py-1 rounded-full text-center flex justify-center items-center cursor-pointer"
+                          >
+                            Edit
+                          </button>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          <button
+                            onClick={() => handleDeleteAnalysis(analysis._id)}
+                            className="bg-red-500 text-white px-4 py-1 rounded-full text-center flex justify-center items-center cursor-pointer"
+                          >
+                            Delete
+                          </button>
                         </td>
                       </tr>
                     ))}
